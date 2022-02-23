6d02d7fa8888b97153f64d4d30c7d5845c5b5c8d
Hentci <1222bruce@gmail.com>
2022-02-23 14:25:57 +0800

    giveUP can't use python = =

---
+++ 1001.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define ar array
+#define sz(x) ((int)x.size())
+template <class T,class S> inline bool chmin(T &a,const S &b) {return (a> b? a= b, 1: 0);}
+template <class T,class S> inline bool chmax(T &a,const S &b) {return (a< b? a= b, 1: 0);}
+int main(){
+    ios_base::sync_with_stdio(false),cin.tie(0);
+    double s;
+    int n;
+    while(cin >> s >> n){
+        
+    }
+
+
+    return 0;
+}
